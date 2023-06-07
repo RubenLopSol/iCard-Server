6ca2b2c903e08d9b310cdac5c01316815c2b4bab
Ruben Lopez <rubenlopsol86@gmail.com>
2023-06-07 12:26:27 +0200

    pre modelviewset categorias

--- icard/settings.py
+++ icard/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
-from pathlib import Path
+import os
 import datetime
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'corsheaders',
     'users',
+    'categories',
     
 
 ]
@@ -132,7 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -150,3 +154,4 @@ CORS_ALLOW_CREDENTIALS = True
 SIMPLE_JWT = {
     'ACCES_TOKEN_LIFETIME': datetime.timedelta(days=120)
 }
+
