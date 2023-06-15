faee00c4ce65665a2615edc10ab1f8da1b2ccf69
Ruben Lopez <rubenlopsol86@gmail.com>
2023-06-15 21:14:52 +0200

    añadir payment al filtrado de orders

--- orders/api/views.py
+++ orders/api/views.py
@@ -11,5 +11,5 @@ class OrderApiViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = ['table', 'status', 'close']
+    filterset_fields = ['table', 'status', 'close', 'payment']
     ordering_fields = '__all__'
